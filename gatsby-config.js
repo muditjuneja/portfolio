2e8a80bec2064bcf8c44cab9e3ae16e6b2ecbe62
Mudit Juneja <muditjuneja@outlook.com>
2021-05-16 01:38:44 +0530

    Changed image placements

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
             title: `Contact`,
             slug: `/contact`,
           },
-          
+
         ],
         externalLinks: [
           {
@@ -96,16 +96,16 @@ module.exports = {
         theme_color: `#6B46C1`,
         display: `standalone`,
         icons: [
-          // {
-          //   src: `/android-chrome-192x192.png`,
-          //   sizes: `192x192`,
-          //   type: `image/png`,
-          // },
-          // {
-          //   src: `/android-chrome-512x512.png`,
-          //   sizes: `512x512`,
-          //   type: `image/png`,
-          // },
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
         ],
       },
     },
